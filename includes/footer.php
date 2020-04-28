d93b9b81fa07d36370d1a1d53bbeab8ac78e0392
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-04-28 16:55:41 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -85,7 +85,7 @@
     function upload() {
         var title_ = document.getElementById("form_title").value;
         var vurl_ = document.getElementById("form_vurl").value;
-        var vtag_ = document.getElementById("tag").value;
+  
     //get your select image
     var image=document.getElementById("image").files[0];
     //now get your image name
@@ -118,7 +118,7 @@
             console.log(title_);
             console.log(vurl_);
             console.log(downlaodURL);
-            console.log(vtag_);
+         
             $.ajax({
                     url:'insert.php',
                     method:'POST',
